0e84d25ed281efbf3ffc7c08fbdc0d6e4e7958fe
kinfen <kinfen.in@gmail.com>
2014-12-28 00:27:58 +0800

    整理模板

--- routes/views/contentManager.js
+++ routes/views/contentManager.js
@@ -8,39 +8,27 @@ exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 	
-	// Set locals
-	locals.section = 'sitecore';
-	
-	// // Load the galleries by sortOrder
-	// view.query('galleries', keystone.list('Gallery').model.find().sort('sortOrder'));
 
-
- 
 	Category.model.find()
     .exec(function(err, categorys) {
-        // 对帖子做些处理
         if (err)
         {
         	req.flash('error', err);
         }
         else
         {
-        	var menuData = [];
         	var hashData = {};
         	for (var i = 0; i < categorys.length; i++)
         	{
         		var obj = categorys[i];
-        		var tmp = {}
+				var tmp = {};
+				tmp._id = obj._id;
         		tmp.text = obj.name;
-        		tmp.href = '#kinfen';
-        		tmp._id = obj._id + '';
-
         		var tag = obj.parent ? obj.parent + '' : 'root';
         		tmp.parent = tag;
         		if (hashData[tag] == null)
         		{
-        			hashData[tag] = new Array();
-        			
+        			hashData[tag] = [];
         		}
         		hashData[tag].push(tmp);
         		// console.log('create array ' + hashData[tag].length);
@@ -64,7 +52,7 @@ exports = module.exports = function(req, res) {
 					var obj = list[i];
 
 					obj.nodes = reformData(obj._id, res);
-					console.log('nodes' + obj.nodes);
+					
 				}
 
 				// console.log(list);
