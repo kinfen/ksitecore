30f19dc1e034c422e12586be8b3ce371fe77a2e9
kinfen <kinfen.in@gmail.com>
2015-03-16 08:15:03 +0800

    no message

---
+++ public/js/table_config/list.js
@@ -0,0 +1,11 @@
+//bootstrap table column options
+
+var list_table_columns = [{
+	    field: '_id',
+	    title: 'Item ID',
+	    visible: false
+	}, {
+	    field: 'name',
+	    title: 'Item Name',
+	    width: 100
+}];
