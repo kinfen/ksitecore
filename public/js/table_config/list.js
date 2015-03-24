1d95d5bb60dc8f91e4f76b4adc02446964cf34fc
kinfen <kinfen.in@gmail.com>
2015-03-24 09:16:11 +0800

    no message

--- public/js/table_config/list.js
+++ public/js/table_config/list.js
@@ -1,11 +1,20 @@
 //bootstrap table column options
 
 var list_table_columns = [{
+		field: "selected",
+		checkbox: true
+	},{
 	    field: '_id',
 	    title: 'Item ID',
 	    visible: false
 	}, {
 	    field: 'name',
 	    title: 'Item Name',
-	    width: 100
-}];
+	},{
+		field: 'operate',
+		title: 'Item Operate',
+		formatter : "operateFormatter",
+		clickToSelect : false,
+		events : "operateEvents"
+	}
+];
